fd860e3601db04703ac169c7ecd930758e3aa29b
Jakub Hozak <jakub.hozak@gmail.com>
2015-06-02 14:25:20 +0200

    * admin tests compatible with friendly ID

--- lib/generators/barbecue/controller/templates/test.rb
+++ lib/generators/barbecue/controller/templates/test.rb
@@ -30,13 +30,13 @@ class <%= controller_class_name %>ControllerTest < ActionController::TestCase
   end
 
   test "update" do
-    patch :update, id: <%= "@#{singular_name}, #{singular_name}: attributes_for(:#{singular_name})" %>
+    patch :update, id: <%= "@#{singular_name}.id, #{singular_name}: attributes_for(:#{singular_name})" %>
     assert_response :success, response.body.to_s
   end
 
   test "destroy" do
     assert_difference('<%= model_class_name %>.count', -1) do
-      delete :destroy, id: <%= "@#{singular_name}" %>
+      delete :destroy, id: <%= "@#{singular_name}" %>.id
       assert_response :success, response.body.to_s
     end
   end
