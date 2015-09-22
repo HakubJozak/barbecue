e32275b884e82b43068e2f9c6b90773a3a52dbd9
Jakub Hozak <jakub.hozak@gmail.com>
2015-09-22 13:11:25 +0200

    F require

--- lib/barbecue.rb
+++ lib/barbecue.rb
@@ -1,16 +1,20 @@
 require_relative "barbecue/version"
 require_relative "barbecue/engine"
+require_relative "barbecue/controller"
+require_relative "barbecue/thumbnails"
+
 require_relative "barbecue/generators"
 require_relative "barbecue/generators/generated_attribute"
 require_relative "barbecue/generators/plain_attribute"
 require_relative "barbecue/generators/string_attribute"
 require_relative "barbecue/generators/image_attribute"
 require_relative "barbecue/generators/images_attribute"
+
 require_relative "barbecue/blueprint"
 require_relative 'barbecue/blueprint/attribute'
 require_relative 'barbecue/blueprint/model'
 require_relative 'barbecue/blueprint/builder'
-require_relative "barbecue/controller"
+
 
 
 module Barbecue
